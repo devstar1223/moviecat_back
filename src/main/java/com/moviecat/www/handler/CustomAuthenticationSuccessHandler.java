4aef12ab52d00f1cc06d5993215ca79162520fc4
devmango1128 <devmango1128@gmail.com>
2024-06-07 19:15:04 +0900

    로그인 handler에 데이터 return 되도록 추가

--- src/main/java/com/moviecat/www/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/moviecat/www/handler/CustomAuthenticationSuccessHandler.java
@@ -1,5 +1,7 @@
 package com.moviecat.www.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moviecat.www.util.MvcUserDetails;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,12 +9,27 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
         response.setStatus(HttpServletResponse.SC_OK); // 성공 상태 코드 200 반환
-        response.getWriter().write("{\"message\": \"Login successful\"}");
-        response.getWriter().flush();
+        MvcUserDetails userDetails = (MvcUserDetails) authentication.getPrincipal();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("mbrId", userDetails.getUsername());
+        data.put("nickNm", userDetails.getNickNm());
+        data.put("profileUrl", userDetails.getAtchFileUrl());
+        data.put("token", userDetails.getToken());
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(data));
     }
 }
--- src/main/java/com/moviecat/www/util/MvcUserDetails.java
+++ src/main/java/com/moviecat/www/util/MvcUserDetails.java
@@ -9,12 +9,16 @@ import java.util.Collection;
 @Getter
 public class MvcUserDetails extends User {
 
+    private final String mbrId;
     private final String nickNm;
+    private final String atchFileUrl;
     private final String token;
 
-    public MvcUserDetails(String mbrId, String pswd, Collection<? extends GrantedAuthority> auth, String nickNm, String token) {
+    public MvcUserDetails(String mbrId, String pswd, Collection<? extends GrantedAuthority> auth, String nickNm, String atchFileUrl, String token) {
         super(mbrId, pswd, auth);
+        this.mbrId = mbrId;
         this.nickNm = nickNm;
         this.token = token;
+        this.atchFileUrl = atchFileUrl;
     }
 }
